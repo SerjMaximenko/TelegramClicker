fa3b15561ce9dd65c67cb4c2df2589b2c6166310
smaksimenko <smaksimenko@moysklad.ru>
2024-06-30 12:28:43 +0300

    master init spring boot

---
+++ src/main/java/com/maksimenko/Clicker/controller/ClientController.java
@@ -0,0 +1,37 @@
+package com.maksimenko.Clicker.controller;
+
+import com.maksimenko.Clicker.model.Client;
+import com.maksimenko.Clicker.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping(value = "/clients")
+    public ResponseEntity<?> create(@RequestBody Client client) {
+        clientService.create(client);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/clients")
+    public ResponseEntity<List<Client>> read() {
+        final List<Client> clients = clientService.readAll();
+
+        return new ResponseEntity<>(clients, HttpStatus.OK);
+    }
+}
